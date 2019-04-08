59d047bda397ad48ed7ca04741b9481b79b0a638
Yuefeng Zhu <git@gansi.me>
2019-04-08 16:23:55 +0800

    fix: Fix graphQL return type err (#33)
    * fix: Fix graphQL return type err and add Access-Control-Allow-Origin *;
    
    * fix(sosconf/utils.py): Fix request_photo_url return avatar bug

--- sosconf/utils.py
+++ sosconf/utils.py
@@ -72,7 +72,7 @@ def request_photo_url(username, event="download"):
     except:
         return "http://www.liberaldictionary.com/wp-content/uploads/2018/11/test-1.png"
     else:
-        if 'err' in json.loads(result).keys() == True:
+        if 'err' in json.loads(result).keys():
             return json.loads(result)["event_url"]
         else:
             return "http://www.liberaldictionary.com/wp-content/uploads/2018/11/test-1.png"
